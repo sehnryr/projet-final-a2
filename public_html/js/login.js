e5114de60cb71f2b129fce8ce35046a0ce6c7bfe
PaulAdrPENET <pauladrienpenet@gmail.com>
2022-06-15 15:05:18 +0200

    js login utils

---
+++ public_html/js/login.js
@@ -0,0 +1,31 @@
+import { createCookie, getCookie } from './utils.js'
+
+// onload if session cookie exists, redirect to user.html
+$(() => {
+	let cookie = getCookie('mm_session')
+	if (cookie.length > 0) {
+		$.ajax("api.php/user", {
+			method: "GET", headers: {
+				Authorization: 'Bearer ' + cookie
+			}
+		}).done((_) => {
+			let url = window.location.href.replace(/login\.html.*/i, 'user.html')
+			window.location.href = url
+		})
+	}
+})
+
+
+$("#loginForm").on("submit", (event) => {
+	event.preventDefault()
+	$.ajax("api.php/login", {
+		method: "POST", data: {
+			email: $("#emailLogin").val(),
+			password: $("#passwordLogin").val()
+		}
+	}).done((data) => {
+		createCookie('mm_session', data['access_token'])
+		let url = window.location.href.replace(/login\.html.*/i, 'user.html')
+		window.location.href = url
+	})
+})
---
+++ public_html/js/register.js
@@ -0,0 +1,27 @@
+import { createCookie, getCookie } from './utils.js'
+
+// onload if session cookie exists, redirect to user.html
+$(() => {
+	let cookie = getCookie('mm_session')
+	if (cookie.length > 0) {
+		let url = window.location.href.replace(/register\.html.*/i, 'user.html')
+		window.location.href = url
+	}
+})
+
+$('#formRegister').on('submit', (event) => {
+	event.preventDefault()
+	$.ajax('api.php/register', {
+		method: 'POST', data: {
+			firstname: $("#firstnameRegister").val(),
+			lastname: $("#lastnameRegister").val(),
+			email: $("#emailRegister").val(),
+			phone: $("#phoneRegister").val(),
+			password: $("#passwordRegister").val()
+		}
+	}).done((data) => {
+		createCookie('mm_session', data['access_token'])
+		let url = window.location.href.replace(/register\.html.*/i, 'user.html')
+		window.location.href = url
+	})
+})
