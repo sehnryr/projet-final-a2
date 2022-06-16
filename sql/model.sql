f6181860a1a22f79cc8250f8fd5614f0b339848d
Youn Mélois <youn@melois.dev>
2022-06-16 11:52:11 +0200

    extend phone_number length in case

--- sql/model.sql
+++ sql/model.sql
@@ -44,7 +44,7 @@ CREATE TABLE "user"(
     "first_name" VARCHAR(64) NOT NULL,
     "last_name" VARCHAR(64) NOT NULL,
     "email" VARCHAR(64) UNIQUE NOT NULL,
-    "phone_number" VARCHAR(20),
+    "phone_number" VARCHAR(30),
     "password_hash" VARCHAR(60) NOT NULL,
     "profile_picture_url" VARCHAR(255),
     "birthdate" DATE NOT NULL,
