6d4debb9c009d23ecbf5e0200147c4fb57e54dbe
sehnryr <youn@melois.dev>
2022-06-23 19:05:30 +0200

    add base

--- public_html/js/explore.js
+++ public_html/js/explore.js
@@ -1,11 +1,11 @@
-import { createCookie, getCookie } from './utils.js'
+// import { createCookie, getCookie } from './utils.js'
 
-$("#match-add-info").on("submit",(event) =>{
-    let current_match = $("#popup-match-id").val()
-    $.ajax("api.php/sport", {
-        
-    })
-});
+// $("#match-add-info").on("submit", (event) => {
+//     let current_match = $("#popup-match-id").val()
+//     $.ajax("api.php/sport", {
+
+//     })
+// });
 /*
 $.ajax("api.php/sport", {
 
@@ -62,3 +62,68 @@ $(() => {
     }
 })
 */
+
+var layerGroup
+var sports = {
+    1: 'Football',
+    2: 'Basketball',
+    3: 'Ping pong',
+    4: 'Badminton',
+    5: 'Volleyball',
+    6: 'Rugby'
+}
+
+$(() => {
+    var map = L.map("map", {
+        center: [48.85341, 2.3488], //Centered on Paris by default
+        zoom: 8,
+    })
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        //Add the openstreetmap tile layer to the map
+        maxZoom: 19,
+    }).addTo(map)
+
+    // L.marker([47.21725, -1.55336]).addTo(map).bindPopup("<b>Hello world!</b><br>I am a popup.")
+    layerGroup = L.layerGroup().addTo(map);
+
+    $.ajax('api.php/matches', {
+        method: 'GET',
+        data: {
+            range: 30
+        }
+    }).done((data) => {
+        layerGroup.clearLayers();
+        data.forEach(element => {
+            L.marker([element.latitude, element.longitude]).addTo(layerGroup)
+                .bindPopup('<p>' + sports[element.sport_id] + '</p>')
+        });
+    })
+})
+
+$('#searchForm').on('submit', (event) => {
+    event.preventDefault()
+
+    let data = {}
+
+    let range = event.originalEvent.submitter.attributes.name.value.split('-').at(-1)
+    if (range == 'searchMatch') {
+        range = 30
+    }
+    data['range'] = range
+
+    let date = $('#match_date').val()
+    if (date) {
+        data['date'] = date
+    }
+
+    $.ajax('api.php/matches', {
+        method: 'GET',
+        data: data
+    }).done((data) => {
+        layerGroup.clearLayers();
+        data.forEach(element => {
+            L.marker([element.latitude, element.longitude]).addTo(layerGroup)
+                .bindPopup('<p>' + sports[element.sport_id] + '</p>')
+        });
+    })
+})
